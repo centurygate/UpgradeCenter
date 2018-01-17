2dab1c121a10cf628cf2c997dbe5c3576fa23031
CenturyGate <centurygate@163.com>
2018-01-17 14:16:30 +0800

    [ADD]1.增加系统升级服务代码和压缩文件

---
+++ routes/mi.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var fs = require('fs');
+
+var router = express.Router();
+console.log('dirname = '+__dirname);
+var pkginfoPath='D:/NodeEx/UpgradeCenter/public/mi/upgrade/pkg.json';
+/* GET home page. */
+function getpackageInfo() {
+    var pkginfo = JSON.parse(fs.readFileSync(pkginfoPath));
+    console.log(JSON.stringify(pkginfo,null,4));
+    return pkginfo;
+}
+var pkginfo = getpackageInfo();
+
+router.get('/pkginfo', function(req, res, next) {
+    console.log("Enter mi pkginfo");
+
+    var result = {};
+    result.status = 'ok';
+    result.reason = 'successful';
+    result.checkSum='';
+    result.version ='';
+    result.fileSize = 0;
+    try
+    {
+        result.checkSum = pkginfo.checkSum;
+        result.version = pkginfo.version;
+        result.fileSize = pkginfo.fileSize;
+        console.log('checkSum = '+result.checkSum);
+    }
+    catch (err)
+    {
+        result.status = 'err';
+        result.reason = ''+err;
+    }
+    res.end(JSON.stringify(result));
+
+});
+
+
+router.get('/checkSum', function(req, res, next) {
+    console.log("Enter mi checkSum");
+
+    var result = {};
+    result.status = 'ok';
+    result.reason = 'successful';
+    result.checkSum='';
+    try
+    {
+        result.checkSum = pkginfo.checkSum;
+        console.log('checkSum = '+result.checkSum);
+    }
+    catch (err)
+    {
+        result.status = 'err';
+        result.reason = ''+err;
+    }
+    res.end(JSON.stringify(result));
+
+});
+
+router.get('/checkNewVersion', function(req, res, next) {
+    console.log("Enter mi checkNewVersion");
+
+    var result = {};
+    var version='';
+    result.status = 'ok';
+    result.reason = 'successful';
+    result.version='';
+    try
+    {
+        result.version = pkginfo.version;
+        console.log('version = '+result.version);
+    }
+    catch (err)
+    {
+        result.status = 'err';
+        result.reason = ''+err;
+    }
+    res.end(JSON.stringify(result));
+});
+
+module.exports = router;
